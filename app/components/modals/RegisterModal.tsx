cd26f7ec36ad9b0318183cb0f00a572c9a1e969c
James <james.auble@gmail.com>
2023-05-18 20:12:30 -0700

    finished adding cloudinary for rentmodal info image step

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -37,9 +37,9 @@ const RegisterModal = () => {
     axios
       .post('/api/register', data)
       .then(() => {
-        toast.success('Registered!');
-        registerModal.onClose();
-        loginModal.onOpen();
+        toast.success('Registered!')
+        registerModal.onClose()
+        loginModal.onOpen()
       })
       .catch((error) => {
         toast.error(error)
--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -13,7 +13,6 @@ interface NavbarProps {
 }
 
 const Navbar: React.FC<NavbarProps> = ({ currentUser }) => {
-  console.log({ currentUser })
   return (
     <div className="fixed z-10 w-full bg-white shadow-sm">
       <div className="border-b py-4">
